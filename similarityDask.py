c3bc937bd8012c1f5018d2df7af3bad899921b59
Jose Miguel Garrido <jm_garrido@hotmail.com>
2018-11-16 00:44:52 +0100

    * Some changes and experiments with stack and agg

--- similarityDask.py
+++ similarityDask.py
@@ -7,6 +7,7 @@ from math import sqrt
 import time
 import dask.dataframe as dd
 from dask.distributed import Client
+import sqlite3
 
 def computeCosineSimilarity(ratingPairs):
     x = ratingPairs["rating_1"]
@@ -81,26 +82,27 @@ def computeMoviePairSimilarities(fileName):
     
     return df
 
+def moviePairsToSQL(conn,mps):
+    conn.execute('''CREATE TABLE IF NOT EXISTS moviePairs
+             (movie1 INTEGER, movie2 INTEGER, numPairs INTEGER, score REAL)''')
+    conn.execute('''DROP INDEX IF EXISTS idx_movie1;''')
+    conn.execute('''DROP INDEX IF EXISTS idx_movie2;''')
+    conn.execute('''CREATE INDEX idx_movie1 ON moviePairs(movie1);''')
+    conn.execute('''CREATE INDEX idx_movie2 ON moviePairs(movie2);''')
+    conn.commit()
+    
+    for i in mps.itertuples(name=None):
+        #print(i)
+        conn.execute('''INSERT INTO moviePairs(movie1,movie2,numPairs,score)
+VALUES(?,?,?,?);''',(i[0][0],i[0][1],i[2],i[1]))
+    conn.commit()
 
-#Main
-if __name__ == "__main__":
-    client = Client(processes=False)
-    print(client)
-    
-    fileRatings = "ml-100k/u.data"
-    fileNames = "ml-100k/u.item"
-    
-    moviePairSimilarities = computeMoviePairSimilarities(fileRatings)
-    
-    #moviePairSimilarities.to_cvs("similarities.json")
-    
+
+
+def searchMovie(moviePairSimilarities,fileNames, movieID = 50, scoreThreshold = 0.97, coOccurenceThreshold = 50):    
     movieNames = pd.read_table(fileNames,names = ["movieID","title"],usecols = ["movieID","title"],
                         sep ="|",index_col = "movieID", encoding = "cp1252")
-    movieNames.head()
     
-    movieID = 50
-    scoreThreshold = 0.97
-    coOccurenceThreshold = 50
     
     filteredResults = moviePairSimilarities.query(
         "((movieID_1 == @movieID) or (movieID_1 == @movieID)) and (score>@scoreThreshold) and (numPairs>@coOccurenceThreshold)"
@@ -114,3 +116,24 @@ if __name__ == "__main__":
             recommended = i[0]
         nR = movieNames.loc[recommended,"title"]
         print(nR,v["score"])
+
+
+
+#Main
+if __name__ == "__main__":
+    client = Client(processes=False)
+    print(client)
+    
+    fileRatings = "ml-100k/u.data"
+    fileNames = "ml-100k/u.item"
+    
+    moviePairSimilarities = computeMoviePairSimilarities(fileRatings)
+    
+    #moviePairSimilarities.to_cvs("similarities.json")
+
+    conn = sqlite3.connect('movieNamesDask.db')
+
+    moviePairsToSQL(conn,moviePairSimilarities)
+
+    conn.close()
+
