85f6ba651720bf0151cdee87c27d8bad460eba6a
Jose Miguel Garrido <jm_garrido@hotmail.com>
2018-11-20 01:07:37 +0100

    added license

--- similarityDask.py
+++ similarityDask.py
@@ -1,5 +1,20 @@
 # coding: utf-8
 
+# (c) Jose Miguel Garrido
+# Based on algorithm from https://www.udemy.com/taming-big-data-with-apache-spark-hands-on/ from Frank Kane
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import pandas as pd
 import numpy as np
